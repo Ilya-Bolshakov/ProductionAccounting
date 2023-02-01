fac7c16ae4b71eed28805876c17782a37b2fd838
Илья <gamenest926@gmail.com>
2023-02-01 19:31:48 +0300

    Добавление IoC, DI

--- ProductionAccounting/App.xaml.cs
+++ ProductionAccounting/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using ProductionAccounting.Registrators;
+using ProductionAccounting.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,34 @@ namespace ProductionAccounting
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _host;
+
+        public static IHost Host => _host ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Microsoft.Extensions.Hosting.Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(
+                 (hostContext, services) => services
+                    //.AddDatabase(hostContext.Configuration.GetSection("Database"))
+                    //.AddServices()
+                    .AddViewModels())
+            ;
+
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
---
+++ ProductionAccounting/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionAccounting.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private string? _title = "Test string";
+
+        public string? Title
+        {
+            get { return _title; }
+            set { Set(ref _title, value); }
+        }
+
+    }
+}
