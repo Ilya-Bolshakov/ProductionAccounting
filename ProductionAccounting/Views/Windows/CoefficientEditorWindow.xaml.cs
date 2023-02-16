529dcd15819f38501847517ce4b2a2c8b50d9556
Илья <gamenest926@gmail.com>
2023-02-16 22:16:12 +0300

    Добавление/редактирование коэффициента

--- ProductionAccounting/Views/Windows/CoefficientEditorWindow.xaml.cs
+++ ProductionAccounting/Views/Windows/CoefficientEditorWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ProductionAccounting.Views.Windows
 {
@@ -22,6 +10,22 @@ namespace ProductionAccounting.Views.Windows
         public CoefficientEditorWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            App.CurrentWindow.Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                DragMove();
         }
     }
 }
