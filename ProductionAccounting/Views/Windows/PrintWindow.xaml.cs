efccbf95b33c78ad34281bdf6842982855f4e4f3
Илья <gamenest926@gmail.com>
2023-02-23 17:18:54 +0300

    Напильником по дизайну

--- ProductionAccounting/Views/Windows/PrintWindow.xaml.cs
+++ ProductionAccounting/Views/Windows/PrintWindow.xaml.cs
@@ -23,5 +23,21 @@ namespace ProductionAccounting.Views.Windows
         {
             InitializeComponent();
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            App.CurrentWindow.Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+                DragMove();
+        }
     }
 }
