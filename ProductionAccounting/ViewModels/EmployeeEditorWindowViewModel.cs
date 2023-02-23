d9149b3e4c9d4dc9ff48499f64218f37390b63a8
Илья <gamenest926@gmail.com>
2023-02-24 01:14:33 +0300

    Стиль для валидированного текстбокса, валидация для сотрудников

--- ProductionAccounting/ViewModels/EmployeeEditorWindowViewModel.cs
+++ ProductionAccounting/ViewModels/EmployeeEditorWindowViewModel.cs
@@ -1,16 +1,27 @@
 ﻿using MathCore.WPF.ViewModels;
 using ProductionAccounting.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace ProductionAccounting.ViewModels
 {
-    public class EmployeeEditorWindowViewModel : ViewModel
+    public class EmployeeEditorWindowViewModel : ViewModel, INotifyDataErrorInfo
     {
+        private readonly Dictionary<string, List<string>> _propertyErrors = new Dictionary<string, List<string>>();
         private string _name;
         public string Name
         {
             get { return _name; }
             set
             {
+                ClearErrors(nameof(Name));
+                if (value != null && value.Contains("w"))
+                {
+                    AddError(nameof(Name), "Тест валидации");
+                }
                 Set(ref _name, value);
             }
         }
@@ -26,6 +37,9 @@ namespace ProductionAccounting.ViewModels
         }
 
         private string _patronymic;
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
         public string Patronymic
         {
             get { return _patronymic; }
@@ -35,6 +49,8 @@ namespace ProductionAccounting.ViewModels
             }
         }
 
+        public bool HasErrors => _propertyErrors.Any();
+
         public EmployeeEditorWindowViewModel(EmployeeModel employee)
         {
             Name = employee.Name;
@@ -42,9 +58,33 @@ namespace ProductionAccounting.ViewModels
             Patronymic = employee.Patronymic;
         }
 
-        public EmployeeEditorWindowViewModel()
+        public IEnumerable GetErrors(string? propertyName)
         {
+            return _propertyErrors.GetValueOrDefault(propertyName, null);
+        }
 
+        public void AddError(string propertyName, string errorMessage)
+        {
+            if (!_propertyErrors.ContainsKey(propertyName))
+            {
+                _propertyErrors.Add(propertyName, new List<string>());
+            }
+
+            _propertyErrors[propertyName].Add(errorMessage);
+            OnErrorsChanged(propertyName);
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            if (_propertyErrors.Remove(propertyName))
+            { 
+                OnErrorsChanged(propertyName);
+            }
         }
     }
 }
