3377454583c10d650786f2dee5e0437240035e3f
Илья <gamenest926@gmail.com>
2023-04-06 22:25:35 +0300

    Проверка интернета

---
+++ ProductionAccounting/Services/Internet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionAccounting.Services
+{
+    public static class Internet
+    {
+        public const string DOMAIN_NAME = "google.com";
+        public static bool IsWork()
+        {
+            try
+            {
+                Dns.GetHostEntry(DOMAIN_NAME);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
